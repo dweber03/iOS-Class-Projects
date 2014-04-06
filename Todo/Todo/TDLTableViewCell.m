9b36a025d6af782380dc802ed0ab7b5f40ca5cca
Derek Weber <derek.weber03@gmail.com>
2014-04-06 14:27:59 -0400

    Todo

--- Todo/Todo/TDLTableViewCell.m
+++ Todo/Todo/TDLTableViewCell.m
@@ -65,8 +65,15 @@
 -(void) awakeFromNib
 {
     
+    
 }
 
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
 
 
 @end
