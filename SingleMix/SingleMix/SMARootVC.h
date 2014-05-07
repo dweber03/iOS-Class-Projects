8062a8800ce08bfd0f9c51bde73453854aedd9ac
Derek Weber <derek.weber03@gmail.com>
2014-05-07 13:32:02 -0400

    SMA - Add root VC

---
+++ SingleMix/SingleMix/SMARootVC.h
@@ -0,0 +1,13 @@
+//
+//  SMARootVC.h
+//  SingleMix
+//
+//  Created by Derek Weber on 5/7/14.
+//  Copyright (c) 2014 Derek Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMARootVC : UIViewController
+
+@end
