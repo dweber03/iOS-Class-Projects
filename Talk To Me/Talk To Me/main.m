20e03db212deaec9aecdaf7885d680f25ad6af9b
Derek Weber <derek.weber03@gmail.com>
2014-06-02 09:23:53 -0400

    TTM App
    -created app that uses parse for push notifications
    -dragged Parse framework into TTM app

---
+++ Talk To Me/Talk To Me/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Talk To Me
+//
+//  Created by Derek Weber on 6/2/14.
+//  Copyright (c) 2014 Derek Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTMAppDelegate class]));
+    }
+}
---
+++ Talk To Me/Talk To MeTests/Talk_To_MeTests.m
@@ -0,0 +1,34 @@
+//
+//  Talk_To_MeTests.m
+//  Talk To MeTests
+//
+//  Created by Derek Weber on 6/2/14.
+//  Copyright (c) 2014 Derek Weber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Talk_To_MeTests : XCTestCase
+
+@end
+
+@implementation Talk_To_MeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
